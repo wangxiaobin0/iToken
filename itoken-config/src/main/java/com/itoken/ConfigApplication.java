8eb7256a81ab02c4d6ff45adebfeb83baf609d4e
wangxb <1378975974@qq.com>
2020-03-16 16:20:37 +0800

    配置中心

---
+++ itoken-config/src/main/java/com/itoken/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.itoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author
+ * @date 2020/3/16
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
